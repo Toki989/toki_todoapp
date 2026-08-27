793b32a0520c346dffaf9746c3e2f8193123d789
LAPTOP-BN9N76GQ\academia <tokibs@icloud.com>
2026-08-27 14:06:45 +0900

    パスワード保護

--- src/main/java/com/example/todoapp/TodoService.java
+++ src/main/java/com/example/todoapp/TodoService.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class TodoService {
 
     private final TodoMapper todoMapper;
@@ -23,13 +26,16 @@ public class TodoService {
 
     public void create(Todo todo) {
         todoMapper.insert(todo);
+        log.info("登録 id={}", todo.getId());
     }
 
     public void update(Todo todo) {
         todoMapper.update(todo);
+        log.info("編集 id={}", todo.getId());
     }
 
     public void delete(Long id) {
         todoMapper.deleteById(id);
+        log.info("削除 id={}", id);
     }
 }
